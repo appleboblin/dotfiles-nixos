00f29412db0f58e1edc49af72a93a001ab277f3b
appleboblin <appleboblin@proton.me>
2024-01-02 21:54:30 +0800

    formatting

--- home-manager/programs/vscodium.nix
+++ home-manager/programs/vscodium.nix
@@ -1,9 +1,9 @@
 {pkgs, inputs, ...}: {
   config = {
     programs.vscode = {
-    enable = true;
-    package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
+      enable = true;
+      package = pkgs.vscodium;
+      extensions = with pkgs.vscode-extensions; [
         arcticicestudio.nord-visual-studio-code
         bbenoist.nix
         njpwerner.autodocstring
@@ -16,12 +16,15 @@
         ms-python.vscode-pylance
         oderwat.indent-rainbow
         pkief.material-product-icons
-    ];
-    userSettings = {
+      ];
+      userSettings = {
         "editor.fontSize" = 16;
+        "window.zoomLevel" = 0;
+        "terminal.integratec.fontSize" = 16;
+        "markdown.preview.fontSize" = 16;
         "workbench.colorTheme" = "Nord";
         "workbench.productIconTheme" = "material-product-icons";
-    };
+      };
     };
   };
 }
