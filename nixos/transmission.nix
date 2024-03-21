c50ea873aa2ca7cac66a47cbd326273827c97e83
appleboblin <appleboblin@proton.me>
2024-03-21 19:33:56 +0800

    transmission gui

--- nixos/transmission.nix
+++ nixos/transmission.nix
@@ -7,5 +7,10 @@
     services.transmission = {
         enable = true; # optional
         package = pkgs.transmission_4;
+        settings = {
+            download-dir = "${config.services.transmission.home}/Downloads";
+        };
+        
     };
+    environment.systemPackages = with pkgs; [ transmission_4-gtk ];
 }
