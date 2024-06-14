72a7184bbf115958d88b335f7c2a8ced647ced08
appleboblin <appleboblin@proton.me>
2024-06-14 20:12:54 +0800

    templates and formatting

--- flake.nix
+++ flake.nix
@@ -1,73 +1,73 @@
 {
-  description = "Nixos config flake";
+    description = "Nixos config flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "github:nixos/nixpkgs/23.11";
-    nixpkgs_22_11.url = "github:nixos/nixpkgs/nixos-22.11";
-    spicetify-nix.url = "github:the-argus/spicetify-nix";
-    # nur.url = "github:nix-community/NUR";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      # inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      # url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    user = "appleboblin";
-    mkHost = host:
-      nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit (nixpkgs) lib;
-          inherit inputs host user;
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        # nixpkgs.url = "github:nixos/nixpkgs/23.11";
+        nixpkgs_22_11.url = "github:nixos/nixpkgs/nixos-22.11";
+        spicetify-nix.url = "github:the-argus/spicetify-nix";
+        # nur.url = "github:nix-community/NUR";
+        nixos-hardware.url = "github:NixOS/nixos-hardware";
+        nixvim = {
+        url = "github:nix-community/nixvim";
+        # inputs.nixpkgs.follows = "nixpkgs";
         };
-        modules = [
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-
-              extraSpecialArgs = {
-                inherit inputs host user;
-                isLaptop = host == "framework";
-                isVm = host == "vm";
-                isDesktop = host == "desktop";
-              };
-
-              users.${user} = {
-                imports = [
-                  # common home-manager configuration
-                  ./home-manager
-                  # host specific home-manager configuration
-                  ./hosts/${host}/home.nix
-                ];
-              };
-            };
-          }
-          (nixpkgs.lib.mkAliasOptionModule ["hm"] ["home-manager" "users" "appleboblin"])
-          ./nixos
-          ./hosts/${host}/configuration.nix
-          ./hosts/${host}/hardware-configuration.nix
-          # inputs.home-manager.nixosModules.main
-        ];
-      };
-  in {
-    nixosConfigurations = {
-      framework = mkHost "framework";
-      vm = mkHost "vm";
-      desktop = mkHost "desktop";
+        home-manager = {
+        url = "github:nix-community/home-manager";
+        # url = "github:nix-community/home-manager/release-23.11";
+        inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-  };
+
+    outputs = {
+        self,
+        nixpkgs,
+        ...
+    } @ inputs: let
+        user = "appleboblin";
+        mkHost = host:
+        nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = {
+            inherit (nixpkgs) lib;
+            inherit inputs host user;
+            };
+            modules = [
+            inputs.home-manager.nixosModules.home-manager
+            {
+                home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+
+                extraSpecialArgs = {
+                    inherit inputs host user;
+                    isLaptop = host == "framework";
+                    isVm = host == "vm";
+                    isDesktop = host == "desktop";
+                };
+
+                users.${user} = {
+                    imports = [
+                    # common home-manager configuration
+                    ./home-manager
+                    # host specific home-manager configuration
+                    ./hosts/${host}/home.nix
+                    ];
+                };
+                };
+            }
+            (nixpkgs.lib.mkAliasOptionModule ["hm"] ["home-manager" "users" "appleboblin"])
+            ./nixos
+            ./hosts/${host}/configuration.nix
+            ./hosts/${host}/hardware-configuration.nix
+            # inputs.home-manager.nixosModules.main
+            ];
+        };
+    in {
+        nixosConfigurations = {
+        framework = mkHost "framework";
+        vm = mkHost "vm";
+        desktop = mkHost "desktop";
+        };
+    };
 }
---
+++ templates/python/flake.nix
@@ -0,0 +1,39 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        devenv.url = "github:cachix/devenv";
+    };
+
+    outputs =
+        inputs@{ flake-parts, nixpkgs, ... }:
+        flake-parts.lib.mkFlake { inherit inputs; } {
+            imports = [ inputs.devenv.flakeModule ];
+            systems = nixpkgs.lib.systems.flakeExposed;
+
+            perSystem =
+                { config, self', inputs', pkgs, system, ...}:
+                {
+                # template from iynaix
+                # Per-system attributes can be defined here. The self' and inputs'
+                # module parameters provide easy access to attributes of the same
+                # system.
+                devenv.shells.default = {
+                    # https://devenv.sh/reference/options/
+                    packages = with pkgs; [ ];
+
+                    dotenv.disableHint = true;
+                    languages.python = {
+                        enable = true;
+                        # Change pemplate if needed
+                        package = pkgs.python3.withPackages (
+                            ps: with ps; [
+                            flake8
+                            black
+                            ]
+                        );
+                        venv.enable = true;
+                    };
+                };
+                };
+    };
+}
