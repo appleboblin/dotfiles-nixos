0ff6d60ed592070a99aa166913781e269a66fd64
appleboblin <appleboblin@proton.me>
2024-06-14 19:22:23 +0800

    latest kernel, shutdown bug, not shutting down properly, led still on after shutdown

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -12,7 +12,8 @@
 	# Bootloader.
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
-	
+	boot.kernelPackages = pkgs.linuxPackages_latest;
+
 	networking.hostName = host; # Define your hostname.
 	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
