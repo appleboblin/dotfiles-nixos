827b12d5a19182570315d81133fb2cee383575d8
appleboblin <github@appleboblin.com>
2025-03-26 15:10:59 +0800

    zed as git editor
    zed as git editor

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -177,7 +177,7 @@
     #  /etc/profiles/per-user/appleboblin/etc/profile.d/hm-session-vars.sh
     #
     sessionVariables = {
-      EDITOR = "zeditor";
+      EDITOR = "zeditor -w";
       BROWSER = "vivaldi";
       TERMINAL = "xterm-256color";
     };
