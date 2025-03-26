acf94be5514a201e54ca943cdcd01aae2313ac9a
appleboblin <github@appleboblin.com>
2025-03-26 17:55:45 +0800

    removed extra bash command

--- home-manager/shell/bash.nix
+++ home-manager/shell/bash.nix
@@ -1,18 +1,18 @@
 {
   programs.bash = {
     enable = true;
-    bashrcExtra = ''
-      function gt () {
-      cd $HOME/dotfiles-nixos/
-      if [[ $# -eq 1 ]]; then
-          cd $1;
-      fi
-      }
-      _gt () {
-      ( cd "$HOME/dotfiles-nixos"; printf "%s\n" "$2"* )
-      }
-      complete -o nospace -C _gt gt
-    '';
+    # bashrcExtra = ''
+    #   function gt () {
+    #   cd $HOME/dotfiles-nixos/
+    #   if [[ $# -eq 1 ]]; then
+    #       cd $1;
+    #   fi
+    #   }
+    #   _gt () {
+    #   ( cd "$HOME/dotfiles-nixos"; printf "%s\n" "$2"* )
+    #   }
+    #   complete -o nospace -C _gt gt
+    # '';
     enableCompletion = true;
   };
 }
